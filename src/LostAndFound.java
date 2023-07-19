75a272cc9b32aad938fb2247110dcc5cffb9d781
Lance Munyao <munyaolance1@gmail.com>
2023-07-19 09:24:52 +0300

    :)

---
+++ src/LostAndFound.java
@@ -0,0 +1,222 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LostAndFound extends JFrame implements ActionListener {
+    private JPanel panel1;
+    private JPanel login;
+    private JPanel registration;
+    private JPanel admin;
+    private JTextField txtLoginUsername;
+    private JTextField txtLoginPassword;
+    private JLabel username;
+    private JLabel password;
+    private JButton btnLoginSubmit;
+    private JTextField txtRegistrationName;
+    private JTextField txtRegistrationSchoolID;
+    private JTextField txtRegistrationEmail;
+    private JTextField txtRegistrationPassword;
+    private JTextField txtRegistrationFaculty;
+    private JTextField txtRegistrationNationalID;
+    private JTextField txtRegistrationAddress;
+    private JTextField txtRegistrationGender;
+    private JTextField txtRegistrationPhone;
+    private JTextField txtRegistrationRole;
+    private JButton btnRegistrationSubmit;
+    private JLabel lblRegName;
+    private JLabel lblRegSchoolID;
+    private JLabel lblRegEmail;
+    private JLabel lblRegPassword;
+    private JLabel lblRegFaculty;
+    private JLabel lblRegNationalID;
+    private JLabel lblRegAddress;
+    private JLabel lblRegGender;
+    private JLabel lblRegPhone;
+    private JLabel lblRegRole;
+    private JPanel student;
+    private JButton btnAdminView;
+    private JButton btnAdminUpdate;
+    private JButton btnAdminDelete;
+    private JButton btnAdminAdd;
+    private JTextField txtStudItem;
+    private JTextField txtAminLocation;
+    private JTextField txtAdminDate;
+    private JTextField txtAdminColor;
+    private JTextField txtAdminType;
+    private JTextField txtAdminInfo;
+    private JTextField txtAdmintemID;
+    private JLabel lblStudItemID;
+    private JLabel lblStudItem;
+    private JLabel lblStudLoca;
+    private JLabel lblStudDate;
+    private JLabel lblStudColor;
+    private JLabel lblStudType;
+    private JLabel lblStudInfo;
+    private JPanel user;
+
+    public void setItem_id(int int1) {
+        this.txtAdmintemID.setText(String.valueOf(int1));
+    }
+
+    public void setItem(String string) {
+        this.txtStudItem.setText(string);
+    }
+
+    public void setLocation(String string) {
+        this.txtAminLocation.setText(string);
+    }
+
+    public void setDate(String string) {
+        this.txtAdminDate.setText(string);
+    }
+
+    public void setColor(String string) {
+        this.txtAdminColor.setText(string);
+    }
+
+    public void setType(String string) {
+        this.txtAdminType.setText(string);
+    }
+
+    public void setAdditional_info(String string) {
+        this.txtAdminInfo.setText(string);
+    }
+
+
+    database db = new database();
+
+
+    public LostAndFound(){
+        this.setTitle("Lost and Found");
+        this.setSize(600,600);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Add the panel to the frame
+        this.add(panel1);
+
+        // Add action listener to the buttons
+        btnLoginSubmit.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == btnLoginSubmit){
+            try{
+                String username = txtLoginUsername.getText();
+                String password = txtLoginPassword.getText();
+                if(db.login(username, password)){
+                    JOptionPane.showMessageDialog(null,"You have logged in successfully.\n Welcome");
+                    login.setVisible(false);
+                    if(db.getRole(username) == "admin"){
+                        admin.setVisible(true);
+                    } else if(db.getRole(username) == "user"){
+                        student.setVisible(true);
+                    } else{
+                        JOptionPane.showMessageDialog(null,"You have not entered all the fields or Wrong credentials");
+                    }
+                } else{
+                    JOptionPane.showMessageDialog(null,"You have not entered all the fields or Wrong credentials");
+                }
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        if(e.getSource() == btnRegistrationSubmit) {
+            try{
+                String name = txtRegistrationName.getText();
+                int school_id = Integer.parseInt(txtRegistrationSchoolID.getText());
+                String email = txtRegistrationEmail.getText();
+                String password = txtRegistrationPassword.getText();
+                String faculty = txtRegistrationFaculty.getText();
+                int national_id = Integer.parseInt(txtRegistrationNationalID.getText());
+                String address = txtRegistrationAddress.getText();
+                String gender = txtRegistrationGender.getText();
+                int phone = Integer.parseInt(txtRegistrationPhone.getText());
+                String role = txtRegistrationRole.getText();
+
+                if(db.register(name, school_id, email, password, faculty, national_id, address, gender, phone, role)){
+                    JOptionPane.showMessageDialog(null,"You have registered successfully.\n Welcome");
+                    registration.setVisible(false);
+                    login.setVisible(true);
+                } else{
+                    JOptionPane.showMessageDialog(null,"You have not entered all the fields or Wrong credentials");
+                }
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        if(e.getSource() == btnAdminAdd){
+            try{
+                String item = txtStudItem.getText();
+                String location = txtAminLocation.getText();
+                String date = txtAdminDate.getText();
+                String color = txtAdminColor.getText();
+                String type = txtAdminType.getText();
+                String info = txtAdminInfo.getText();
+
+                if(db.add(item, location, date, color, type, info)){
+                    JOptionPane.showMessageDialog(null,"You have added an item successfully.\n Welcome");
+                    admin.setVisible(false);
+                    student.setVisible(true);
+                } else{
+                    JOptionPane.showMessageDialog(null,"You have not entered all the fields or Wrong credentials");
+                }
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        if(e.getSource() == btnAdminDelete){
+            try{
+                int item_id = Integer.parseInt(txtAdmintemID.getText());
+
+                if(db.delete(item_id)){
+                    JOptionPane.showMessageDialog(null,"You have deleted an item successfully.\n Welcome");
+                    admin.setVisible(false);
+                    student.setVisible(true);
+                } else{
+                    JOptionPane.showMessageDialog(null,"You have not entered all the fields or Wrong credentials");
+                }
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        if(e.getSource() == btnAdminUpdate){
+            try{
+                int item_id = Integer.parseInt(txtAdmintemID.getText());
+                String item = txtStudItem.getText();
+                String location = txtAminLocation.getText();
+                String date = txtAdminDate.getText();
+                String color = txtAdminColor.getText();
+                String type = txtAdminType.getText();
+                String info = txtAdminInfo.getText();
+
+                if(db.update(item_id, item, location, date, color, type, info)){
+                    JOptionPane.showMessageDialog(null,"You have updated an item successfully.\n Welcome");
+                    admin.setVisible(false);
+                    student.setVisible(true);
+                } else{
+                    JOptionPane.showMessageDialog(null,"You have not entered all the fields or Wrong credentials");
+                }
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        if(e.getSource() == btnAdminView){
+        try{
+            db.getLostAndFound();
+
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
+
+}
+
+
---
+++ src/database.java
@@ -0,0 +1,235 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+public class database {
+    static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://localhost:3306/lost_and_found";
+    static final String USER = "root";
+    static final String PASSWORD = "";
+    private Statement statement;
+    private Connection connection;
+
+
+    // The login function
+    public boolean login(String username, String password) {
+        boolean success = false;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            statement = connection.createStatement();
+
+            String query = "SELECT * FROM tbl_attendant WHERE attendant_username = '" +
+                    username + "' AND attendant_password = '" + password + "'";
+            System.out.println(query);
+            ResultSet rs = statement.executeQuery(query);
+            success = rs.next();
+            rs.close();
+            //
+
+            statement.close();
+            connection.close();
+            if(success){
+                JOptionPane.showMessageDialog(null,"You have logged in successfully.\n Welcome");
+            } else{
+                JOptionPane.showMessageDialog(null,"You have not entered all the fields or Wrong credentials");
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+        return success;
+    }
+
+
+    // The register function to the registration table
+    public boolean register(String name, int school_id, String email, String password, String faculty, int national_id, String address, String gender, int phone, String role) {
+        boolean success = false;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            statement = connection.createStatement();
+            String query = "INSERT INTO registration (name, school_id, email, password, faculty, national_id, address, gender, phone, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, school_id);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, password);
+            preparedStatement.setString(5, faculty);
+            preparedStatement.setInt(6, national_id);
+            preparedStatement.setString(7, address);
+            preparedStatement.setString(8, gender);
+            preparedStatement.setInt(9, phone);
+            preparedStatement.setString(10, role);
+            
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                success = true;
+            }
+            
+            preparedStatement.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return success;
+    }
+
+    // Use username to get the user role in registration table and store the role in a variable
+    public String getRole(String username) {
+        String role = "";
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            statement = connection.createStatement();
+
+            String query = "SELECT role FROM registration WHERE username = '" + username + "'";
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                role = rs.getString("role");
+            }
+            rs.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return role;
+    }
+
+    // The add function to the lost and found table
+    public boolean add(String item, String Location, String Date, String Color, String Type, String Additional_info){
+        boolean success = false;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            statement = connection.createStatement();
+            String query = "INSERT INTO lost_and_found (item, Location, Date, Color, Type, Additional_info) VALUES (?, ?, ?, ?, ?, ?)";
+            
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, item);
+            preparedStatement.setString(2, Location);
+            preparedStatement.setString(3, Date);
+            preparedStatement.setString(4, Color);
+            preparedStatement.setString(5, Type);
+            preparedStatement.setString(6, Additional_info);
+            
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                success = true;
+            }
+            
+            preparedStatement.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return success;
+    }
+
+    // delete function to the lost and found table using the id
+    public boolean delete(int Item_id){
+        boolean success = false;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            statement = connection.createStatement();
+            String query = "DELETE FROM lost_and_found WHERE Item_id = ?";
+            
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, Item_id);
+            
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                success = true;
+            }
+            
+            preparedStatement.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return success;
+    }
+
+    // Update function with id included
+    public boolean update(int Item_id, String item, String Location, String Date, String Color, String Type, String Additional_info){
+        boolean success = false;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            statement = connection.createStatement();
+            String query = "UPDATE lost_and_found SET item = ?, Location = ?, Date = ?, Color = ?, Type = ?, Additional_info = ? WHERE Item_id = ?";
+            
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, item);
+            preparedStatement.setString(2, Location);
+            preparedStatement.setString(3, Date);
+            preparedStatement.setString(4, Color);
+            preparedStatement.setString(5, Type);
+            preparedStatement.setString(6, Additional_info);
+            preparedStatement.setInt(7, Item_id);
+            
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                success = true;
+            }
+            
+            preparedStatement.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return success;
+    }
+
+    // Fetch data from the lost and found table and store it in a list
+    public List<LostAndFound> getLostAndFound() {
+        List<LostAndFound> lostAndFound = new ArrayList<>();
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            statement = connection.createStatement();
+
+            String query = "SELECT * FROM lost_and_found";
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                LostAndFound lost = new LostAndFound();
+                lost.setItem_id(rs.getInt("Item_id"));
+                lost.setItem(rs.getString("item"));
+                lost.setLocation(rs.getString("Location"));
+                lost.setDate(rs.getString("Date"));
+                lost.setColor(rs.getString("Color"));
+                lost.setType(rs.getString("Type"));
+                lost.setAdditional_info(rs.getString("Additional_info"));
+                lostAndFound.add(lost);
+            }
+            rs.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lostAndFound;
+    }
+
+
+}
+
+    
+
+
+
